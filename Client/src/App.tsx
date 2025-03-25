c5c5ccc14c073396fc0d739a7ab63f743f618e57
Gaurav Kapur <gauravkapur596@gmail.com>
2025-03-26 03:26:20 +0530

    Redirect / to /home

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,19 +1,20 @@
 import { Signin } from "./pages/Signin";
 import { Signup } from "./pages/Signup";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Dashboard } from "./pages/dashboard"; // Adjusted to match your casing
+import { Dashboard } from "./pages/dashboard";
 import { Home } from "./pages/Home";
-import { SharePage } from "./pages/SharePage"; // Import the new page
+import { SharePage } from "./pages/SharePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Home />} />
+        {/* Serve Home at root */}
+        <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/brain/:shareLink" element={<SharePage />} /> {/* New route */}
+        <Route path="/brain/:shareLink" element={<SharePage />} />
       </Routes>
     </BrowserRouter>
   );
